d63faf9c85626a098503135f0b915bac79b324a0
anders rindal Loeng <anderrl@stud.ntnu.no>
2020-12-17 11:27:45 +0100

    first

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,96 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  constructor() {
+    super();
+      
+    this.user = { //this.user should contain the information that was sent from users-list when triggered
+      uname: '',
+      firstName: '',
+      lastName: '',
+      oldPwd: '',
+      pwd: ''
+
+
+
+      }
+  }
+
+  static styles = css`
+    .forms {
+      padding: 20px 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    `;
+
+  render () {
+    return html`
+
+    <form name="form">
+      <div class = "forms">
+        <label for="username"> username </label>
+        <input name="username" placeholder="${user.uname}"/>
+        
+      </div>
+      <div class = "forms">
+        <label for="fistname"> Firstname </label>
+        <input name="firstname" placeholder="${user.firstname}"/>
+        
+      </div>
+      <div class = "forms">
+        <label for="lastname"> Lastname </label>
+        <input name="username" placeholder="${user.lastname}"/>
+        
+      </div>
+      <div class = "forms">
+        <label for="password"> Passwrod </label>
+        <input name="password" placeholder="${user.oldPwd}"/>
+        
+      </div>
+
+      <div class="forms"> 
+        <button @click=${this.checkFields}>
+        Submit
+        </button>
+
+      </div>
+
+    </form>
+
+
+
+  }
+
+checkFields() { //The ide of this function is to fist check if there is a new password, and to check for which information the user wants to update
+  var u = document.forms["form"]["username"].value;
+  var f = document.forms["form"]["firstname"].value;
+  var l = document.forms["form"]["lastname"].value;
+  var p = document.forms["form"]["password"].value;
+
+  if (p == null) {
+
+    oldPwd = ${this.user.pwd}
+
+    fetch('api/updateUser.php', {method: 'POST'})
+      .then(respons) => //send information from form to the api, and there it will update the database
+
+  }
+
+  else { //Old password is set as the current password, and the password is changed to be the password typed into the form
+
+    oldPwd = ${this.user.pwd}; 
+    Pwd = p;
+
+    fetch('api/updateUser.php', {method: 'POST'})
+      .then(respons) => //Add the infromation into the database
+
+  }
+
+}
+
 
 }
 customElements.define('edit-user', EditUser);
